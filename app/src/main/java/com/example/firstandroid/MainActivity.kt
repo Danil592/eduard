818c3867c009b4aba1fdc38b7cc8dd9fbc9d9c53
Danil592 <dangorets@gmail.com>
2025-03-29 20:24:24 +0200

    Cherez ZH , no rabotaet

--- app/src/main/java/com/example/firstandroid/MainActivity.kt
+++ app/src/main/java/com/example/firstandroid/MainActivity.kt
@@ -1,22 +1,45 @@
 package com.example.firstandroid
 
-import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
+import com.example.firstandroid.databinding.NewLayoutFileBinding
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    private lateinit var binding: NewLayoutFileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.newlayoutfile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = NewLayoutFileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.containerFirstAnswer.setOnClickListener {
+            makeUncorrect(binding.containerFirstAnswer)
         }
+        binding.containerSecondAnswer.setOnClickListener {
+            makeUncorrect(binding.containerSecondAnswer)
+        }
+        binding.containerThirdAnswer.setOnClickListener {
+            makeUncorrect(binding.containerThirdAnswer)
+        }
+        binding.containerFourthAnswer.setOnClickListener {
+            makeCorrect(binding.containerFourthAnswer)
+        }
+        binding.btnSkip.setOnClickListener { makeNeutral() }
+
+    }
+    private fun makeCorrect(container: LinearLayout) {
+        container.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+    }
+    private fun makeUncorrect(container:LinearLayout) {
+        container.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+    }
+    private fun makeNeutral() {
+        binding.containerFirstAnswer.setBackgroundColor(Color.WHITE)
+        binding.containerSecondAnswer.setBackgroundColor(Color.WHITE)
+        binding.containerThirdAnswer.setBackgroundColor(Color.WHITE)
+        binding.containerFourthAnswer.setBackgroundColor(Color.WHITE)
     }
 }
