68e639d788ea2c55fd0f64ccee617a3b4d660a3a
Danil592 <dangorets@gmail.com>
2025-03-29 21:16:32 +0200

    another pravkas

--- app/src/main/java/com/example/firstandroid/MainActivity.kt
+++ app/src/main/java/com/example/firstandroid/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.firstandroid
 
 import android.graphics.Color
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -15,19 +16,25 @@ class MainActivity : AppCompatActivity() {
         binding = NewLayoutFileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         binding.containerFirstAnswer.setOnClickListener {
             makeUncorrect(binding.containerFirstAnswer)
+            showResult(false)
         }
         binding.containerSecondAnswer.setOnClickListener {
             makeUncorrect(binding.containerSecondAnswer)
+            showResult(false)
         }
         binding.containerThirdAnswer.setOnClickListener {
             makeUncorrect(binding.containerThirdAnswer)
+            showResult(false)
         }
         binding.containerFourthAnswer.setOnClickListener {
             makeCorrect(binding.containerFourthAnswer)
+            showResult(true)
         }
-        binding.btnSkip.setOnClickListener { makeNeutral() }
+        binding.btnContinue.setOnClickListener { makeNeutral() }
 
     }
     private fun makeCorrect(container: LinearLayout) {
@@ -41,5 +48,22 @@ class MainActivity : AppCompatActivity() {
         binding.containerSecondAnswer.setBackgroundColor(Color.WHITE)
         binding.containerThirdAnswer.setBackgroundColor(Color.WHITE)
         binding.containerFourthAnswer.setBackgroundColor(Color.WHITE)
+    }
+    private fun showResult(IsCorrect: Boolean){
+        var color:Int
+        var image:Int
+        var text:String
+        if(IsCorrect){
+            color = ContextCompat.getColor(this, R.color.green)
+            image= R.drawable.ic_icon
+            text = "Correct"
+        }else {
+            color = ContextCompat.getColor(this, R.color.red)
+            image= R.drawable.ic_wrong
+            text = "Wrong"
+        }
+        binding.ResultContainer.setBackgroundColor(color)
+        binding.finger.setImageResource(image)
+        binding.correctWrong.setText(text)
     }
 }
